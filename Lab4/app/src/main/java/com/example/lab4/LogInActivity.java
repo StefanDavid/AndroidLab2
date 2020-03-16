040397e495a31040a920a85d0ff5dbf9deaf74e2
Stefan <stefanvaleanu@yahoo.com>
2020-03-16 18:06:23 +0200

    update
    Lab 3 and 4

---
+++ Lab4/app/src/main/java/com/example/lab4/LogInActivity.java
@@ -0,0 +1,45 @@
+package com.example.lab4;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Context;
+import android.content.Intent;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.LinearLayout;
+
+public class LogInActivity extends AppCompatActivity {
+    EditText e1;
+    EditText e2;
+    Button btn;
+    Context context;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_log_in);
+
+        context = getApplicationContext();
+        e1 = findViewById(R.id.edit1);
+        e2 = findViewById(R.id.edit2);
+        btn = findViewById(R.id.btn);
+
+        btn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent myIntent = new Intent(context, MainActivity.class);
+                myIntent.addFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
+
+                if(e1.getText().toString().equals("stefan") && e2.getText().toString().equals("1234")){
+
+                    Log.d("$$$", "ceva");
+                    context.startActivity(myIntent);}
+            }
+        });
+
+
+    }
+}
